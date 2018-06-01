b200d9e1afe76ac1995c3dc1112613ca3fe2d935
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-02 07:23:44 +0800

    Designed and created endpoints for API. All passed tests in Postman except the PUT methods for Projects and Actions.

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ const nameCheckMiddleware = (req, res, next) => {
 
 // ===== PROJECTS ENDPOINTS =====
 
+server.post('/api/projects', (req, res) => {
+    const { name, description } = req.body;
+    projects
+        .insert({ name, description })
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
 server.get('/api/projects', (req,res) => {
     projects
         .get()
@@ -37,13 +49,125 @@ server.get('/api/projects', (req,res) => {
         });
 });
 
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects
+        .get(id)
+        .then(userProjects => {
+            if(userProjects === 0) {
+                return errorHelper(404, 'No projects by that user', res);
+            }
+            res.json(userProjects);
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
+server.put('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    projects
+        .update(id, { name })
+        .then(response => {
+            if (response === 0) {
+                return errorHelper(404, 'No project by that ID.');
+            } else {
+                db.find(id).then(projects => {
+                    res.json(projects);
+                });
+            }
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
+server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects
+        .remove(id)
+        .then(projectRemoved => {
+            if (projectRemoved === 0) {
+                return errorHelper(404, 'No project by that ID.');
+            } else {
+                res.json({ success: 'Project removed'});
+            }
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
 // ===== ACTIONS ENDPOINTS =====
+
+server.post('/api/actions', (req, res) => {
+    const { project_id, description, notes } = req.body;
+    actions
+        .insert({ project_id, description, notes })
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
 
 server.get('/api/actions', (req,res) => {
     actions
         .get()
         .then(foundActions => {
             res.json(foundActions);
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions
+        .get(id)
+        .then(userActions => {
+            if(userActions === 0) {
+                return errorHelper(404, 'No projects by that user', res);
+            }
+            res.json(userActions);
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
+server.put('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    const { project_id, description, notes } = req.body;
+    actions
+        .update(id, { project_id, description, notes })
+        .then(response => {
+            if (response === 0) {
+                return errorHelper(404, 'No project by that ID.');
+            } else {
+                db.find(id).then(projects => {
+                    res.json(projects);
+                });
+            }
+        })
+        .catch(err => {
+            return errorHelper(500, 'Internal server error', res);
+        });
+});
+
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions
+        .remove(id)
+        .then(actionRemoved => {
+            if (actionRemoved === 0) {
+                return errorHelper(404, 'No project by that ID.');
+            } else {
+                res.json({ success: 'Action removed'});
+            }
         })
         .catch(err => {
             return errorHelper(500, 'Internal server error', res);
